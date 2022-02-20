82945b657e711ffcea7f09268ad0fb8f40e8f117
danqi <74356362+danqi123@users.noreply.github.com>
2022-02-20 21:11:12 +0100

    upload NLP_123 files

---
+++ constant.py
@@ -0,0 +1,7 @@
+"""File Path"""
+
+Drug_Review_Train = "../Data/drugsCom_raw/drugsComTrain_raw.tsv"
+Drug_Review_Test = "../Data/drugsCom_raw/drugsComTest_raw.tsv"
+PubMed_20k_train = "../Data/PubMed_20k/train_20k.txt"
+PubMed_20k_test = "../Data/PubMed_20k/test_20k.txt"
+
